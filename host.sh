997fd8061b83429ad7aedbbd168e212f2021faad
huangvinh1 <31427998+huangvinh1@users.noreply.github.com>
2019-03-18 16:43:37 +0700

    Create host.sh

---
+++ host.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+sudoedit /etc/dhcp/dhclient-exit-hooks.d/hostname
+
+if [ "$interface" != "ens33"]
+then
+  return
+fi
+
+if [ "$reason" != BOUND ] && [ "$reason" != RENEW] \
+  && [ "$reason" != REBIND ] && [ "$reason" != REBOOT ]
+then
+  return
+fi
+
+echo dhclient-exit-hooks.d/hostname: Dynamic IP address =$new_ip_address
+hostname=$(host $new_ip_address | cut -d ' ' -f 5 | sed -r 's/((.*)[^\.])\.?/\1/g' )
+echo $hostname > /etc/hostname
+hostname $hostname
+echo dhclient-exit-hooks.d/hostname: Dynamic Hostname= $hostname
